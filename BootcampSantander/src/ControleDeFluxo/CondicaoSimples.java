a9febb9d94b0a00c18b0b895697d80b054110da6
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-05-09 20:59:36 -0300

    C13 - Adicionando a classe Condição Simples no pacote Controle de Fluxo.

---
+++ BootcampSantander/src/ControleDeFluxo/CondicaoSimples.java
@@ -0,0 +1,19 @@
+package ControleDeFluxo;
+
+public class CondicaoSimples {
+	
+	public static void main(String[] args) {
+		
+		// Quando ocorre uma validação de execução de fluxo somente quando a condição for positiva, consideramos como uma estrutura Simples, exemplo:
+		
+		 double saldo = 25.0;
+	     double valorSolicitado = 17.0;
+
+	     if(valorSolicitado < saldo)
+	     saldo = saldo - valorSolicitado;
+
+	     System.out.println(saldo);
+				
+	}
+
+}
