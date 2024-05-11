013ab5c907783912f8fc1da36b41e3dd827f866f
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-05-10 21:13:55 -0300

    C20- Adicionando a classe FOR 2 no pacote Estruturas de Controle.

---
+++ BootcampSantander/src/EstruturasDeRepeticao/For2.java
@@ -0,0 +1,22 @@
+package EstruturasDeRepeticao;
+
+public class For2 {
+	
+	public static void main(String[] args) {
+		
+		//o que importa é somente o bloco condicional
+		
+		int carneirinhos = 1;
+		
+		for( ; carneirinhos <=20; ) {
+			
+		     System.out.println(carneirinhos + " - Carneirinho(s)");
+		     
+		     carneirinhos ++;
+		}
+
+		//for( somente 1x ; deve ser uma expresão boolean; acontecerá a cada final da execução )
+		
+	}
+
+}
