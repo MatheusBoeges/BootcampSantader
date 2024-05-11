82d6ce4b21c4301ccc8de0758a81f92bba68e964
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-05-11 18:48:33 -0300

    C23 - Adicionando a classe Break no pacote Estruturas de Controle.

---
+++ BootcampSantander/src/EstruturasDeRepeticao/UtilizandoBreak.java
@@ -0,0 +1,19 @@
+package EstruturasDeRepeticao;
+
+public class UtilizandoBreak {
+	
+	public static void main(String[] args) {
+		
+		// Break significa quebrar, parar, frear, interromper. E é isso que se faz quando o Java encontra esse comando pela frente. 
+		
+		for(int numero = 1; numero <=5; numero ++){
+			
+			if(numero==3)
+				break;
+			
+			System.out.println(numero);
+			
+		}
+			
+	}
+}
