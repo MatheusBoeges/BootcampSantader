355fbb6c8cf7c0e8b972f3273c785b51486c0b9a
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-05-22 21:26:36 -0300

    C38 - Definindo modificadores de nivel private.

--- BootcampSantander/src/POO/Lanchonete/atendimento/cozinha/Almoxarife.java
+++ BootcampSantander/src/POO/Lanchonete/atendimento/cozinha/Almoxarife.java
@@ -10,10 +10,10 @@ public class Almoxarife {
 	 * 
 	 */
 	
-	public void controlarEntrada() {
+	private void controlarEntrada() {
 		System.out.println("CONTROLANDO A ENTRADA DOS ITENS");
 	}
-	public void controlarSaida() {
+	private void controlarSaida() {
 		System.out.println("CONTROLANDO A SAIDA DOS ITENS");
 	}
 	
@@ -21,7 +21,7 @@ public class Almoxarife {
 	
 	void entregarIngredientes() {
 		System.out.println("ENTREGANDO INGREDIENTES");
-		//...?
+		controlarSaida();
 	}
 	void trocarGas() {
 		System.out.println("ALMOXARIFE TROCANDO O GÁS");
