b69f63f5727eb5bc15303f3ca9d7026f0af8b691
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-05-25 19:01:10 -0300

    C46 - Adicionando a classe MSN Messenger no pacote POO.

---
+++ BootcampSantander/src/POO/MSNMessenger.java
@@ -0,0 +1,24 @@
+package POO;
+
+/*
+ * Programação orientada a objetos (POO, ou OOP segundo as suas siglas em inglês), é um paradigma de programação baseado no conceito de "objetos",
+ * que podem conter dados na forma de campos, também conhecidos como atributos e códigos, na forma de procedimentos, também conhecidos como métodos.
+ * 
+ */
+
+public class MSNMessenger {
+	
+	public void enviarMensagem() {
+		System.out.println("Enviando mensagem");
+	}
+	public void receberMensagem() {
+		System.out.println("Recebendo mensagem");
+	}
+	public void validarConectadoInternet() {
+		System.out.println("Validando se está conectado a internet");
+	}
+	public void salvarHistoricoMensagem() {
+		System.out.println("Salvando o histórico da mensagem");
+	}
+
+}
