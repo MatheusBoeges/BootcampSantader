d0fadf183018a329f56d377e92583420d0523d39
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-05-31 19:21:23 -0300

    Criando a classe Tarefa no Pacote Collections.

---
+++ BootcampSantander/src/Collections/List/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,14 @@
+package Collections.List.OperacoesBasicas;
+
+public class Tarefa {
+	
+	private String descricao;
+	
+	public Tarefa(String decscricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+}
