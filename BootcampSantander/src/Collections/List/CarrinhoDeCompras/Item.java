53b43eca3c587bf6d03780a9949cc0af6e5df0da
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-06-01 20:52:50 -0300

    C56 - Renomeando a classe Carrinho para Item.

--- BootcampSantander/src/Collections/List/CarrinhoDeCompras/Item.java
+++ BootcampSantander/src/Collections/List/CarrinhoDeCompras/Item.java
@@ -1,12 +1,12 @@
 package Collections.List.CarrinhoDeCompras;
 
-public class Carrinho {
+public class Item {
 	
 	private String nome;
 	private double preco;
 	private int quantidade;
 	
-	public Carrinho(String nome, double preco, int quantidade) {
+	public Item(String nome, double preco, int quantidade) {
 		this.nome = nome;
 		this.preco = preco;
 		this.quantidade = quantidade;
