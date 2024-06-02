12f84833c78de41b4155d65d7535ef6f84e31f13
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-06-02 19:29:56 -0300

    C67 - Criando a classe OrdenacaoPessoas no pacote List.

---
+++ BootcampSantander/src/Collections/List/OrdenacaoPessoa/OrdencaoPessoas.java
@@ -0,0 +1,38 @@
+package Collections.List.OrdenacaoPessoa;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdencaoPessoas {
+
+	private List<Pessoa> pessoasList;
+	
+	public OrdenacaoPessoas() {
+		this.pessoasList = new ArrayList<>();
+	}
+	
+	 public void adicionarPessoa(String nome, int idade, double altura) {
+		    pessoasList.add(new Pessoa(nome, idade, altura));
+		  }
+
+		  public List<Pessoa> ordenarPorIdade() {
+		    List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoasList);
+		    if (!pessoasList.isEmpty()) {
+		      Collections.sort(pessoasPorIdade);
+		      return pessoasPorIdade;
+		    } else {
+		      throw new RuntimeException("A lista está vazia!");
+		    }
+		  }
+
+		  public List<Pessoa> ordenarPorAltura() {
+		    List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoasList);
+		    if (!pessoasList.isEmpty()) {
+		      Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+		      return pessoasPorAltura;
+		    } else {
+		      throw new RuntimeException("A lista está vazia!");
+		    }
+
+}
