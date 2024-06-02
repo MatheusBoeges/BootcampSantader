d7dc9c3adab1ab19626d450a4c34e56380668331
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-06-02 18:08:51 -0300

    C62 - Criando a classe Numero no pacote List.

---
+++ BootcampSantander/src/Collections/List/SomaNumeros/Numeros.java
@@ -0,0 +1,20 @@
+package Collections.List.SomaNumeros;
+
+public class Numeros {
+	
+	private int numero;
+	
+	public Numero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	@Override
+	public String toString() {
+		return "Numeros [numero=" + numero + "]";
+	}
+	
+}
