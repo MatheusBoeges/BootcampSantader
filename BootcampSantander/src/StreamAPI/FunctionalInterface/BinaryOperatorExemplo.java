ea11f8cb7d9bc9651d5a260497a4fe6ec208edb5
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-06-05 22:06:42 -0300

    a

--- BootcampSantander/src/StreamAPI/FunctionalInterface/BinaryOperatorExemplo.java
+++ BootcampSantander/src/StreamAPI/FunctionalInterface/BinaryOperatorExemplo.java
@@ -27,19 +27,6 @@ public class BinaryOperatorExemplo {
 	    // Imprimir o resultado da soma
 	    System.out.println("A soma dos números é: " + resultado);
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 	}
 
 }
