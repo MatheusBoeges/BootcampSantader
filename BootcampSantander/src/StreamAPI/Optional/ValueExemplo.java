aab3322c1fab250b421f8919da357a63c4530d51
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-06-06 21:48:16 -0300

    C114 - Criando a classe ValueExemplo no pacote Optional.

---
+++ BootcampSantander/src/StreamAPI/Optional/ValueExemplo.java
@@ -0,0 +1,19 @@
+package StreamAPI.Optional;
+
+import java.util.Optional;
+
+public class ValueExemplo {
+	
+	/*
+	 * . of(value): Cria um Optional contendo o valor fornecido. Se o valor for nulo, lançará uma exceção NullPointerException.
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		
+		Optional<String> optionalValue = Optional.of("Hello");
+		System.out.println(optionalValue.get());
+		
+	}
+
+}
