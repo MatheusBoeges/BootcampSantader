e8d23d6bd9e431b0f9684334d38bb2fb95f5b44c
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-06-07 21:01:55 -0300

    C123 - Criando a classe IfPresentExemplo no pacote Optional.

---
+++ BootcampSantander/src/StreamAPI/Optional/IfPresentExemplo.java
@@ -0,0 +1,19 @@
+package StreamAPI.Optional;
+
+import java.util.Optional;
+
+public class IfPresentExemplo {
+	
+	/*
+	 * .ifPresent(consumer): Executa uma ação fornecida por um Consumer se o Optional contiver um valor.
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		
+		Optional<String> optionalValue = Optional.of("Hello");
+		optionalValue.ifPresent(value -> System.out.println("Valor presente: " + value));
+		
+	}
+
+}
