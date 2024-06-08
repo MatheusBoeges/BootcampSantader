268f2a4a11b411822ecabbc5afb4c5768eb6d1e4
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-06-07 21:08:47 -0300

    C125 - Criando a classe StreamApi no pacote StreamApi.

---
+++ BootcampSantander/src/StreamAPI/StremAPI.java
@@ -0,0 +1,20 @@
+package StreamAPI;
+
+public class StremAPI {
+	
+	/*
+	 * .A Streams API traz uma nova opção para a manipulação de coleções em Java seguindo os princípios da programação funcional.
+	 * 
+	 * .Stream, trata-se de uma poderosa solução para processar coleções de maneira declarativa, ao invés da tradicional e burocrática forma imperativa.
+	 * 
+	 * .Na forma imperativa, para realizar uma soma simples, por exemplo, o desenvolvedor tem que se preocupar não apenas com o que deve ser feito em cada elemento,
+	 * isto é, com as regras associadas ao processamento dos elementos da lista, mas também com a maneira de realizar essa iteração.
+	 * 
+	 * .Combinada com as Expressões Lambda e Method reference, eles proporcionam uma forma diferente de lidar com conjuntos de elementos,
+	 * oferecendo ao desenvolvedor uma maneira simples e concisa de escrever código que resulta em facilidade de manutenção e paralelização sem efeitos indesejados em tempo de execução.
+	 * 
+	 * .As operações na Stream API podem ser classificadas em duas categorias principais
+	 * 
+	 */
+
+}
