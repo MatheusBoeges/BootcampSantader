689c037f71c963b53b1568884c4fd30c4246279c
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-06-07 21:00:01 -0300

    C122 - Criando a classe OrElseTrowExemplo no pacote Optional.

---
+++ BootcampSantander/src/StreamAPI/Optional/OrElseTrowExemplo.java
@@ -0,0 +1,19 @@
+package StreamAPI.Optional;
+
+import java.util.Optional;
+
+public class OrElseTrowExemplo {
+	
+	/*
+	 * .orElseThrow(exceptionSupplier): Obtém o valor contido no Optional, ou lança uma exceção fornecida por um Supplier se o Optional estiver vazio.
+	 * 
+	 */
+	
+	public static void main(String[] args) {
+		
+		Optional<String> optionalValue = Optional.ofNullable(null);
+		String result = optionalValue.orElseThrow(() -> new RuntimeException("Value not present"));
+				
+	}
+
+}
